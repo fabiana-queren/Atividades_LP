25ada859996f514e7d09a22d35551a40dbe6fe56
fabiana-queren <fabianaqueresf@hotmail.com>
2025-06-10 15:20:28 -0300

    pratica_9 - questao 1 updated

--- pratica_9/empresa_1/empregado.cpp
+++ pratica_9/empresa_1/empregado.cpp
@@ -1,3 +1,18 @@
+/*
+
+Program: [Empresa Management System in C++]
+
+Author: [Fabiana Queren]
+
+Date: [10/06/2025]
+
+Description: [This program demonstrates a simple company management system in C++.
+It includes classes for Person, Employee, and Company, showcasing inheritance and polymorphism. The program creates instances of these classes, performs operations like hiring and paying employees, and manages memory effectively.]
+
+License: [CC-BY]
+
+*/
+
 #include "include/empregado.hpp"
 
 Empregado::Empregado(const std::string& nome, int idade, double salario): Pessoa(nome, idade, cpf), salario(salario) {}
