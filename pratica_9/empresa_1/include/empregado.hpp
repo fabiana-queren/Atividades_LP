e7fb082e234b14635364880aaf7a5baf7b610227
fabiana-queren <fabianaqueresf@hotmail.com>
2025-06-10 15:03:38 -0300

    pratica 9 - questao 1

--- pratica_9/empresa_1/include/empregado.hpp
+++ pratica_9/empresa_1/include/empregado.hpp
@@ -1,3 +1,18 @@
+/*
+
+Program: [Empresa Management System in C++]
+
+Author: [Fabiana Queren]
+
+Date: [10/06/2025]
+
+Description: [This program demonstrates a simple company management system in C++.
+It includes classes for Person, Employee, and Company, showcasing inheritance and polymorphism. The program creates instances of these classes, performs operations like hiring and paying employees, and manages memory effectively.]
+
+License: [CC-BY]
+
+*/
+
 #ifndef EMPREGADO_HPP
 #define EMPREGADO_HPP
 
--- pratica_9/empresa_1/pessoa.cpp
+++ pratica_9/empresa_1/pessoa.cpp
@@ -1,3 +1,17 @@
+/*
+
+Program: [Empresa Management System in C++]
+
+Author: [Fabiana Queren]
+
+Date: [10/06/2025]
+
+Description: [This program demonstrates a simple company management system in C++.
+It includes classes for Person, Employee, and Company, showcasing inheritance and polymorphism. The program creates instances of these classes, performs operations like hiring and paying employees, and manages memory effectively.]
+
+License: [CC-BY]
+
+*/
 #include "include/pessoa.hpp"
 
 Pessoa::Pessoa(const std::string& nome, int idade, int cpf) : nome(nome), idade(idade), cpf(cpf) {}
